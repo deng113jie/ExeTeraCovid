4b57bb1992353261eba561504e680caa05a1b6d4
Ben Murray <ben.murray@gmail.com>
2020-10-30 11:01:19 +0000

    Fixing bugs in setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     name='exeteracovid',
     version=__version__,
     description='Analytics for the Covid Symptom Study, based on ExeTera',
-    url='https://github.com/kcl-bmeis/ExeTeraCovid'
+    url='https://github.com/kcl-bmeis/ExeTeraCovid',
     author='Ben Murray',
     author_email='benjamin.murray@kcl.ac.uk',
     license='http://www.apache.org/licenses/LICENSE-2.0',
-    packages=['exeteracovid, 'exeteracovid.algorithms'],
+    packages=['exeteracovid', 'exeteracovid.algorithms', 'exeteracovid.processing'],
     install_requires=[
         'exetera'
     ]
