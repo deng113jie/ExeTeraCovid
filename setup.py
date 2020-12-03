59ed6462882ebea7ac41999318ae78262d77d966
Ben Murray <ben.murray@gmail.com>
2020-12-03 11:37:09 +0000

    Adding long description to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@ from setuptools import setup
 
 from exeteracovid import __version__
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='exeteracovid',
     version=__version__,
     description='Analytics for the Covid Symptom Study, based on ExeTera',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/kcl-bmeis/ExeTeraCovid',
     author='Ben Murray',
     author_email='benjamin.murray@kcl.ac.uk',
